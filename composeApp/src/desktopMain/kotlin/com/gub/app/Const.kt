02dc8833bc156b235443829788f15c83727a7672
alim <sourav.0.alim@gmail.com>
2025-07-04 23:38:55 +0600

    Back to dev mode (localhost)

--- composeApp/src/desktopMain/kotlin/com/gub/app/Const.kt
+++ composeApp/src/desktopMain/kotlin/com/gub/app/Const.kt
@@ -1,6 +1,6 @@
 package com.gub.app
 
 object Const {
-    const val BASE_URL = "35.240.216.70:8080"
-//        const val BASE_URL = "localhost:8080"
+//    const val BASE_URL = "35.240.216.70:8080"
+        const val BASE_URL = "localhost:8080"
 }
